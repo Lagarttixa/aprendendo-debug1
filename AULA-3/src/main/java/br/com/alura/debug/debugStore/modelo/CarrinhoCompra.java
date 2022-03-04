5e7f4e510921a84e136e42750535d24cb1e64101
Abner Machado <abnermachado@sisconsultoria.com.br>
2022-03-04 11:07:25 -0300

    segunda alteração

--- AULA-3/src/main/java/br/com/alura/debug/debugStore/modelo/CarrinhoCompra.java
+++ AULA-3/src/main/java/br/com/alura/debug/debugStore/modelo/CarrinhoCompra.java
@@ -22,7 +22,23 @@ public class CarrinhoCompra {
 			double porcentagemDesconto = this.getFormaPagamento().getPorcentagemDesconto();
 			BigDecimal valorComDesconto = preco.multiply(new BigDecimal(porcentagemDesconto)).divide(new BigDecimal("100"));
 			BigDecimal valorProdutoComDesconto =  preco.subtract(valorComDesconto);
-			total = total.add(valorProdutoComDesconto);
+			if (valorProdutoComDesconto.compareTo(new BigDecimal("700.00")) == 1 ){
+				System.out.println("O produto " + produto.getDescricao() + " ganhou mais R$20,00 de desconto");
+				valorProdutoComDesconto = valorProdutoComDesconto.subtract(new BigDecimal("20.00"));
+			}
+			BigDecimal multiplicador = new BigDecimal(produto.getQuantidade());
+			total = total.add(valorProdutoComDesconto.multiply(multiplicador));
+		}
+		total = calcularPromocao(produtos, total);
+		return total;
+	}
+	
+	private BigDecimal calcularPromocao(List<Produto> produtos, BigDecimal total) {
+		for (Produto produto : produtos) {
+			int quantidadeDescontar = produto.getQuantidade() / 3;
+			for(int cont = 0; quantidadeDescontar > cont; cont++ ){
+				total = total.subtract(produto.getPreco());
+			}
 		}
 		return total;
 	}
